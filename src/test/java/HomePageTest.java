98771a97a56422a1636f5797fb2f2bba7ec22b7b
Maksim Chubura <chubura.max@yandex.ru>
2023-04-26 09:52:20 +0300

    third_commit

--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 import static org.junit.Assert.assertEquals;
 
@@ -25,25 +25,73 @@ public class HomePageTest {
         //FirefoxOptions options = new FirefoxOptions();
         //driver = new FirefoxDriver(options);
         driver.get("https://qa-scooter.praktikum-services.ru/");
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         HomePage = new HomePage(driver);
         HomePage.closeCookie();
     }
 
     @Test
-    public void checkTestQuestions() {
+    public void checkFirstQuestion() {
         HomePage.scrollQuestions();
-        HomePage.clickQuestions();
-        String text = "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.";
-        assertEquals("Текст не совпадает", text, HomePage.getAnswer());
+        HomePage.clickFirstQuestions();
+        String text = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
+        assertEquals("Текст не совпадает", text, HomePage.getFirstAnswer());
     }
 
     @Test
-    public void checkTestSecondQuestions() {
+    public void checkSecondQuestions() {
         HomePage.scrollQuestions();
-        HomePage.clickSecondQuestions();
-        String text = "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.";
+        HomePage.clickSecondQuestion();
+        String text = "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.";
         assertEquals("Текст не совпадает", text, HomePage.getSecondAnswer());
+    }
+
+    @Test
+    public void checkThirdQuestion() {
+        HomePage.scrollQuestions();
+        HomePage.clickThirdQuestion();
+        String text = "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.";
+        assertEquals("Текст не совпадает", text, HomePage.getThirdAnswer());
+    }
+
+    @Test
+    public void checkFourthQuestion() {
+        HomePage.scrollQuestions();
+        HomePage.clickFourthQuestion();
+        String text = "Только начиная с завтрашнего дня. Но скоро станем расторопнее.";
+        assertEquals("Текст не совпадает", text, HomePage.getFourthAnswer());
+    }
+
+    @Test
+    public void checkFifthQuestion() {
+        HomePage.scrollQuestions();
+        HomePage.clickFifthQuestion();
+        String text = "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.";
+        assertEquals("Текст не совпадает", text, HomePage.getFifthAnswer());
+    }
+
+    @Test
+    public void checkSixthQuestion() {
+        HomePage.scrollQuestions();
+        HomePage.clickSixthQuestion();
+        String text = "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.";
+        assertEquals("Текст не совпадает", text, HomePage.getSixthAnswer());
+    }
+
+    @Test
+    public void checkSeventhQuestion() {
+        HomePage.scrollQuestions();
+        HomePage.clickSeventhQuestion();
+        String text = "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.";
+        assertEquals("Текст не совпадает", text, HomePage.getSeventhAnswer());
+    }
+
+    @Test
+    public void checkEighthQuestion() {
+        HomePage.scrollQuestions();
+        HomePage.clickEighthQuestion();
+        String text = "Да, обязательно. Всем самокатов! И Москве, и Московской области.";
+        assertEquals("Текст не совпадает", text, HomePage.getEighthAnswer());
     }
 
     @After
--- src/test/java/PageObject/HomePage.java
+++ src/test/java/PageObject/HomePage.java
@@ -5,13 +5,15 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.time.Duration;
+
 public class HomePage {
     // добавил поле driver
     private WebDriver driver;
     // локатор для кнопки куки
     private By cookieButton = By.id("rcc-confirm-button");
     //локатор для выпадающего списка в Вопросах о важном
-    private By questions = By.xpath("//*[@id=\"root\"]/div/div/div[5]/div[2]/div");
+    private By questions = By.xpath("//*[@id=\"root\"]/div/div[1]/div[5]/div[2]/div");
 
 
     // метод для закрытия кнопки куки
@@ -27,17 +29,61 @@ public class HomePage {
         ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", element);
     }
     // метод нажимает на выпадающий список
-    public void clickQuestions() {
-        driver.findElement(By.cssSelector("#accordion__heading-4")).click();
+    public void clickFirstQuestions() {
+        driver.findElement(By.cssSelector("#accordion__heading-0")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
-    public void clickSecondQuestions() {
+    public void clickSecondQuestion() {
+        driver.findElement(By.cssSelector("#accordion__heading-1")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+    public void clickThirdQuestion() {
+        driver.findElement(By.cssSelector("#accordion__heading-2")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+    public void clickFourthQuestion() {
+        driver.findElement(By.cssSelector("#accordion__heading-3")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+    public void clickFifthQuestion() {
+        driver.findElement(By.cssSelector("#accordion__heading-4")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+    public void clickSixthQuestion() {
+        driver.findElement(By.cssSelector("#accordion__heading-5")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+    public void clickSeventhQuestion() {
         driver.findElement(By.cssSelector("#accordion__heading-6")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+    public void clickEighthQuestion() {
+        driver.findElement(By.cssSelector("#accordion__heading-7")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
     // метод возвращает отображаемый текст выпадающего списка
-    public String getAnswer() {
-        return driver.findElement(By.cssSelector("#accordion__panel-4")).getText();
+    public String getFirstAnswer() {
+        return driver.findElement(By.cssSelector("#accordion__panel-0")).getText();
     }
     public String getSecondAnswer() {
+        return driver.findElement(By.cssSelector("#accordion__panel-1")).getText();
+    }
+    public String getThirdAnswer() {
+        return driver.findElement(By.cssSelector("#accordion__panel-2")).getText();
+    }
+    public String getFourthAnswer() {
+        return driver.findElement(By.cssSelector("#accordion__panel-3")).getText();
+    }
+    public String getFifthAnswer() {
+        return driver.findElement(By.cssSelector("#accordion__panel-4")).getText();
+    }
+    public String getSixthAnswer() {
+        return driver.findElement(By.cssSelector("#accordion__panel-5")).getText();
+    }
+    public String getSeventhAnswer() {
         return driver.findElement(By.cssSelector("#accordion__panel-6")).getText();
+    }
+    public String getEighthAnswer() {
+        return driver.findElement(By.cssSelector("#accordion__panel-7")).getText();
     }
 }
